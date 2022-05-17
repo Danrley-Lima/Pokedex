70c89fa832ea332a175fba887844e4b32b5d2343
Danrley Lima <danrleylac@gmail.com>
2022-05-17 15:58:34 -0300

    Inserindo app-bar

--- src/pokedex/Pokedex.tsx
+++ src/pokedex/Pokedex.tsx
@@ -3,7 +3,13 @@ import { useState, useEffect } from "react"
 import { getPokemonDetails } from "../pokemon/services/getPokemonDetails"
 import { PokemonDetail } from "../pokemon/services/interfaces/PokemonDetail"
 import { listPokemons, PokemonListInterface } from "../pokemon/services/listPokemons"
-
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
 
 
 export function Pokedex() {
@@ -26,7 +32,26 @@ export function Pokedex() {
 
     return (
         <>
-            <h1>Pokedex</h1>
+            <Box sx={{ flexGrow: 1 }}>
+                <AppBar position="static">
+                    <Toolbar>
+                        <IconButton
+                            size="large"
+                            edge="start"
+                            color="inherit"
+                            aria-label="menu"
+                            sx={{ mr: 2 }}
+                        >
+                            <MenuIcon />
+                        </IconButton>
+                        <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+                            Pokedex
+                        </Typography>
+                        <Button color="inherit">Login</Button>
+                    </Toolbar>
+                </AppBar>
+            </Box>
+
 
             Pokemons: <br></br>
             {pokemons.map((pokemon) => {
